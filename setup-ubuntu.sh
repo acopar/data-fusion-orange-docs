a264fed13fb3354328b078015f0789ebafe2bb12
acopar <andrej.copar@gmail.com>
2015-05-29 14:29:27 +0200

    Add graphviz dependency.

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install git python-pip python-virtualenv python3-dev python3-numpy python3-scipy python3-matplotlib ipython3-notebook libxml2-dev zlib1g-dev python3-pyqt4 python-qt4-dev python3-sip-dev libqt4-dev
+sudo apt-get install git python-pip python-virtualenv python3-dev python3-numpy python3-scipy python3-matplotlib ipython3-notebook libxml2-dev zlib1g-dev python3-pyqt4 python-qt4-dev python3-sip-dev libqt4-dev libgraphviz-dev
 
 mkdir orange3env
 virtualenv -p python3 --system-site-packages orange3env
